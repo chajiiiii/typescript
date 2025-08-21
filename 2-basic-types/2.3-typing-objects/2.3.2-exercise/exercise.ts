d194736f1a4118a621e342fe50f2713d609148c8
chajiiiii <j.cha1708@gmail.com>
2025-08-22 02:37:31 +0900

    Practice - Basic types exercises

--- 2-basic-types/2.3-typing-objects/2.3.2-exercise/exercise.ts
+++ 2-basic-types/2.3-typing-objects/2.3.2-exercise/exercise.ts
@@ -6,21 +6,51 @@
 // - title: string
 // - author: string
 // - pages: number
-
+const book = {
+  title: "책 제목",
+  author: "Jonas",
+  pages: 178,
+};
 
 // 질문 2: book 객체 리터럴에 다른 속성을 추가하려고 하면 TypeScript가 허용할까요?
-
+// 아니요.
 
 // 질문 3: 다음과 같은 속성을 가진 `Car` 인터페이스를 정의하세요.
 // - make: string
 // - model: string
 // - year: number
 // 그런 다음 이 인터페이스를 따르는 객체를 만드세요.
+interface Car {
+  make: string;
+  model: string;
+  year: number;
+}
 
+const car1: Car = {
+  make: "ferrari",
+  model: "ferrari",
+  year: 1984,
+};
 
 // 질문 4: `Car`와 동일한 형태를 가진 타입 별칭 `Car2`를 정의하세요.
 // 그리고 이 타입 별칭을 따르는 객체를 만드세요.
+type Car2 = {
+  make: string;
+  model: string;
+  year: number;
+};
 
+const car2: Car2 = {
+  make: "BMW",
+  model: "BMW",
+  year: 1995,
+};
 
 // 질문 5: `Car`와 동일한 형태를 가지면서,
 // 추가적인 어떤 프로퍼티도 허용하는 `Car3` 인터페이스를 만드세요.
+interface Car3 {
+  make: string;
+  model: string;
+  year: number;
+  [key: string]: unknown;
+}
--- 2-basic-types/2.5-unknown/2.5.2-exercise/exercise.ts
+++ 2-basic-types/2.5-unknown/2.5.2-exercise/exercise.ts
@@ -3,12 +3,15 @@
 // --------------------------------------------------------------------------
 
 // 1. `unknown` 타입의 변수를 선언하고 값을 할당하세요.
-
+const boy: unknown = "shinchan";
 
 // 2. `unknown` 타입의 인자를 받아서, 그 인자가 문자열이면
 //    true를 반환하는 `isString` 함수를 작성하세요.
+function isString(unknownValue: unknown): unknownValue is string {
+  return typeof unknownValue === "string";
+}
 
-
-// 3. 위에서 선언한 `unknownValue`가 문자열인지 
-//    `isString` 함수를 통해 확인하고 만약 문자열이라면, 
+// 3. 위에서 선언한 `unknownValue`가 문자열인지
+//    `isString` 함수를 통해 확인하고 만약 문자열이라면,
 //    그 길이를 콘솔에 출력하세요.
+if (isString(boy)) console.log(boy.length);
