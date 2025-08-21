c49f502c42f4bb2eaf3dec21d920e1c4a4529fa4
chajiiiii <j.cha1708@gmail.com>
2025-08-21 21:10:37 +0900

    first commit

---
+++ 1-introduction/1.6-tsconfig-intro/1.6.1-examples/example.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var message = '안녕! 타입스크립트 컴파일러. 이 파일을 자바스크립트 파일로 컴파일 해줘! 🦁';
+console.log(message);
+//# sourceMappingURL=example.js.map
