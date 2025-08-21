5d1fc500944f54b74459e758868c2aa7d2889ed4
chajiiiii <j.cha1708@gmail.com>
2025-08-22 02:36:57 +0900

    Learn - Add type 'never' example

--- 2-basic-types/2.6-never/2.6.1-examples/example-3.ts
+++ 2-basic-types/2.6-never/2.6.1-examples/example-3.ts
@@ -1,12 +1,29 @@
+// if else 예시
+
 function example(x: string | number) {
-  if (typeof x === 'string') {
-    x.toUpperCase()
-  } else if (typeof x === 'number') {
-    x.toFixed(2)
+  if (typeof x === "string") {
+    x.toUpperCase();
+  } else if (typeof x === "number") {
+    x.toFixed(2);
   } else {
-    // 유니온(union) 값에서 다루는 값이 아닌 경우, 
+    // 유니온(union) 값에서 다루는 값이 아닌 경우,
     // Never 타입이 적용됩니다.
-    const _exhaustiveCheck: never = x 
-    return _exhaustiveCheck
+    const _exhaustiveCheck: never = x;
+    return _exhaustiveCheck;
+  }
+}
+
+// switch 예시
+
+type Fruit = "apple" | "banana" | "orange";
+
+function pickFruit(fruit: Fruit) {
+  switch (fruit) {
+    case "apple":
+    case "banana":
+      return fruit;
+    default:
+      const _exhaustiveCheck: never = fruit; // ← 여기
+      return _exhaustiveCheck;
   }
 }
