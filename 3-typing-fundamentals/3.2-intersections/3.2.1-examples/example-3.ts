67a3e91cb904e252cec6dca1396b7655a476d9ec
chajiiiii <j.cha1708@gmail.com>
2025-08-24 03:20:40 +0900

    Learn - Prettify utility

--- 3-typing-fundamentals/3.2-intersections/3.2.1-examples/example-3.ts
+++ 3-typing-fundamentals/3.2-intersections/3.2.1-examples/example-3.ts
@@ -1,14 +1,14 @@
-type User = { name: string } & { age: number }
+type User = { name: string } & { age: number };
 //   ^?
 
-// Prettify 유틸리티는 
-// 인터섹션 타입을 더 읽기 쉽게 만들어줍니다.
-type Prettify<T> = {
-  [K in keyof T]: T[K]
-} & {}
+// Prettify 유틸리티는
+// 인터섹션 타입을 더 읽기 쉽게 만들어줍니다. (개발자 경험 향상)
+type Prettify<Type> = {
+  [Prop in keyof Type]: Type[Prop];
+} & {};
 
-type PrettyUser = Prettify<User>
+type PrettyUser = Prettify<User>;
 //   ^?
 
-// 또는 "Prettify TypeScript: Better Type Previews" 
+// 또는 "Prettify TypeScript: Better Type Previews"
 // VS Code 확장을 사용할 수도 있습니다.
